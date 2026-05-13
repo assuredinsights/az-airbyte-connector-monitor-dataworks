b3d60b853f2743b020b44f93474765f14bc919de
shalinideviramalingam <shalinidevi.ramalingam@assuredinsights.com>
2026-05-13 15:32:45 +0100

    added changes for ngage

--- function_app.py
+++ function_app.py
@@ -5,14 +5,14 @@ from airbyte_connectors_monitor import run_clients_by_schedule_group
 app = func.FunctionApp()
 
 @app.timer_trigger(schedule="0 0 8 * * 1-5", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
+              use_monitor=True) 
 def airbyte_monitor_daily8am(myTimer: func.TimerRequest) -> None:
     logging.info("Running Airbyte daily 8 am schedule group")
     results = run_clients_by_schedule_group("daily_8am")
     logging.info("Completed Airbyte schedule group daily8am. Results: %s", results)
 
 @app.timer_trigger(schedule="0 0 9 * * 1", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
+              use_monitor=True) 
 def airbyte_monitor_monday_8am(myTimer: func.TimerRequest) -> None:
     logging.info("Running Airbyte schedule group: monday_8am")
     results = run_clients_by_schedule_group("monday_8am")
