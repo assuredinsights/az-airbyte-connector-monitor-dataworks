b5c2343747aaf3b5fcbd741fff1b1a38870431e8
ShalinideviRamalingam <163424626+ShalinideviRamalingam@users.noreply.github.com>
2026-05-13 16:07:45 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -9,14 +9,14 @@ app = func.FunctionApp()
 def airbyte_monitor_daily8am(myTimer: func.TimerRequest) -> None:
     logging.info("Running Airbyte daily 8 am schedule group")
     results = run_clients_by_schedule_group("daily_8am")
-    logging.info("Completed Airbyte schedule group daily8am. Results: %s", results)
+    logging.info("Completed Airbyte schedule group daily8am.Results: %s", results)
 
 @app.timer_trigger(schedule="0 0 9 * * 1", arg_name="myTimer", run_on_startup=False,
               use_monitor=True) 
 def airbyte_monitor_monday_8am(myTimer: func.TimerRequest) -> None:
     logging.info("Running Airbyte schedule group: monday_8am")
     results = run_clients_by_schedule_group("monday_8am")
-    logging.info("Completed Airbyte schedule group monday_8am. Results: %s", results)
+    logging.info("Completed Airbyte schedule group monday_8am.Results: %s", results)
 
 
 # @app.timer_trigger(schedule="0 0 */6 * * *", arg_name="myTimer", run_on_startup=True,
