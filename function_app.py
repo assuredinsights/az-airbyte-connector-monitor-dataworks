334c34dde0a6caad26444defcfc6e25dd4adfcf6
shalinideviramalingam <shalinidevi.ramalingam@assuredinsights.com>
2026-06-05 14:09:03 +0100

    ädded new client-Meininger

--- function_app.py
+++ function_app.py
@@ -18,6 +18,13 @@ def airbyte_monitor_monday_8am(myTimer: func.TimerRequest) -> None:
     results = run_clients_by_schedule_group("monday_8am")
     logging.info("Completed Airbyte schedule group monday_8am. Results: %s", results)
 
+@app.timer_trigger(schedule="0 0 8 * * 1-5", arg_name="myTimer", run_on_startup=False,
+              use_monitor=True) 
+def airbyte_monitor_daily9am(myTimer: func.TimerRequest) -> None:
+    logging.info("Running Airbyte daily 9 am schedule group")
+    results = run_clients_by_schedule_group("daily_9am")
+    logging.info("Completed Airbyte schedule group daily8am. Results: %s", results)
+
 
 # @app.timer_trigger(schedule="0 0 */6 * * *", arg_name="myTimer", run_on_startup=True,
 #               use_monitor=False) 
